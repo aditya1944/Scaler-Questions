029104a5adb83817b34509db3361f73a6f45a2e7
aditya1944 <aditya1944@live.com>
2020-06-28 21:28:10 +0530

    solution for Leetcode question reorder data in log files

---
+++ ReorderDatainLogFiles.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+bool compare(const std::string &log1, const std::string &log2){
+    //determine if log1 and log2 is number or word
+    bool log1IsAlpha = isalpha(log1.back());
+    bool log2IsAlpha = isalpha(log2.back());
+    if(log1IsAlpha==log2IsAlpha){
+        if(!log1IsAlpha){
+            //both are number type
+            return false;
+        }
+        else{
+            //both are word type
+            int index1 = log1.find(' ') + 1;
+            int index2 = log2.find(' ') + 1;
+            std::string suffix1 = log1.substr(index1);
+            std::string suffix2 = log2.substr(index2);
+            if(suffix1!=suffix2){
+                return suffix1<suffix2;
+            }
+            else{
+                return log1<=log2;
+            }
+        }
+    }
+    //they are different
+    if(log1IsAlpha){
+        return true;
+    }
+    return false;
+}
+std::vector<string> reorderLogFiles(std::vector<std::string>& logs) {
+    std::stable_sort(logs.begin(),logs.end(),compare);
+    return logs;
+}
+int main(){
+    std::vector<std::string>  vec = {"dig2 3 6","dig1 8 1 5 1"};
+    vec = reorderLogFiles(vec);
+    for(auto s: vec){
+        cout<<s<<endl;
+    }
+    return 0;
+}
+    
