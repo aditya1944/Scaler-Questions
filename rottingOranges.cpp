8cac5460394961d6d32a8ca19fa0353e2b4a0eb1
aditya1944 <aditya1944@live.com>
2020-06-28 10:58:12 +0530

    Easy solution for rotting oranges

--- rottingOranges.cpp
+++ rottingOranges.cpp
@@ -4,64 +4,62 @@
 struct Node{
     int row,col;
 };
-int solve(std::vector<std::vector<int> >&A){
+int solve(std::vector<std::vector<int> >&grid){
+    //use bfs
+    const int rows = grid.size(),cols = grid[0].size();
     //use bfs
     std::queue<Node> q;
-    //traverse full matrix and insert positions of rotten oranges in the queue
-    const int rows = A.size(),cols = A[0].size();
+    //first insert all the rotten oranges index
+    bool flag = false;
+    //flag is just to check if there is any fresh oranges or not
     for(int rowIndex = 0;rowIndex<rows;++rowIndex){
         for(int colIndex = 0;colIndex<cols;++colIndex){
-            if(A[rowIndex][colIndex]==2){
-                Node node; node.row = rowIndex;node.col = colIndex;
-                q.push(node);
+            int status = grid[rowIndex][colIndex];
+            if(status==2){
+                //this orange is rotten
+                q.push({rowIndex,colIndex});
+            }
+            if(status==1){
+                flag = true;
             }
         }
+    }
+    if(!q.size()){
+        //there is no rotten oranges
+        if(flag){
+            return -1;
+        }
+        return 0;
     }
     int time = 0;
+    std::vector<std::pair<int, int> > dirs = {{0,-1},{-1,0},{0,1},{1,0}};
     while(!q.empty()){
-        int noOfNodeatCurrentLevel = q.size();
-        ++time;
-        while(noOfNodeatCurrentLevel--){
+        int size = q.size();
+        while(size--){
+            //check all direction of rotten oranges and insert it in q
             Node node = q.front();
             q.pop();
-            int rowIndex = node.row,colIndex = node.col;
-            //insert neighbouring nodes in queue;
-
-            //1. up
-            if(rowIndex-1>=0 && A[rowIndex-1][colIndex]==1){
-                A[rowIndex-1][colIndex]=2;
-                Node up; up.row = rowIndex-1;up.col = colIndex;
-                q.push(up);
-            }
-            //2. down
-            if(rowIndex+1<rows && A[rowIndex+1][colIndex]==1){
-                A[rowIndex+1][colIndex]=2;
-                Node down; down.row = rowIndex+1;down.col = colIndex;
-                q.push(down);
-            }
-            //3. left
-            if(colIndex-1>=0 && A[rowIndex][colIndex-1]==1){
-                A[rowIndex][colIndex-1] = 2;
-                Node left; left.row = rowIndex;left.col = colIndex-1;
-                q.push(left);
-            }
-            //4.right
-            if(colIndex+1<cols && A[rowIndex][colIndex+1]==1){
-                A[rowIndex][colIndex+1] = 2;
-                Node left; left.row = rowIndex;left.col = colIndex+1;
-                q.push(left);
+            for(auto dir: dirs){
+                int neighbourRow = node.row + dir.first;
+                int neighbourCol = node.col + dir.second;
+                if(neighbourRow>=0 && neighbourCol>=0 && neighbourRow<rows && neighbourCol<cols){
+                    int status = grid[neighbourRow][neighbourCol];
+                    if(status==1){
+                        q.push({neighbourRow,neighbourCol});
+                        grid[neighbourRow][neighbourCol] = 2;
+                    }
+                }
             }
         }
+        ++time;
     }
-    if(time==0){
-        return -1;
-    }
-    for(auto vec: A){
-        for(int val: vec){
-            if(val==1){
+    for(int rowIndex = 0;rowIndex<rows;++rowIndex){
+        for(int colIndex = 0;colIndex<cols;++colIndex){
+            int status = grid[rowIndex][colIndex];
+            if(status==1){
                 return -1;
             }
         }
     }
-    return time;
+    return time-1;
 }
